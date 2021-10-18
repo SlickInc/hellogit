e85f496e46e3e9a750b90c0836bf81df69b80a00
Ivan <ivennsfroese@gmail.com>
2021-10-18 14:13:36 -0500

    Testing

--- helloGit/src/helloGit/HelloGit.java
+++ helloGit/src/helloGit/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		//a greeting
-		//testing
+		//testing testing
 		//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
 		System.out.println("Hello, Git");
 
